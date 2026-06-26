7f4644a1898d8b10a99068323027dadfeb54576f
Poojanokhwal <poojanokhwal04@gmail.com>
2026-06-26 20:14:52 +0530

    Amazon Checkout workflow

--- TestCase/conftest.py
+++ TestCase/conftest.py
@@ -1,4 +1,4 @@
-from selenium.webdriver import Edge,EdgeOptions
+from selenium.webdriver import Edge, EdgeOptions
 from selenium.webdriver import Firefox,FirefoxOptions
 from selenium.webdriver import Chrome,ChromeOptions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,7 +6,40 @@ from Utilities import ReadConfigini
 from Utilities.ReadScreenshotPath import screenshotpath
 import pytest
 
+from selenium import webdriver
+
+
 @pytest.fixture
+# def setup(request):
+#
+#     url=ReadConfigini.read_configuration_file("common info", "url")
+#     browser=ReadConfigini.read_configuration_file("common info", "browser")
+#
+#     if browser=="firefox":
+#         o = FirefoxOptions()
+#         driver = Firefox(options=o)
+#     elif browser=="edge":
+#         o = EdgeOptions()
+#         o.add_experimental_option('detach', True)
+#         driver = Edge(options=o)
+#     else:
+#         o = ChromeOptions()
+#         o.add_experimental_option('detach', True)
+#         driver = Chrome(options=o)
+#
+#     wait=WebDriverWait(driver,20)
+#     testname = request.node.name
+#     path = screenshotpath(testname)
+#
+#     request.cls.driver=driver
+#     request.cls.wait=wait
+#     request.cls.path=path
+#
+#     driver.get(url)
+#     yield path
+#     driver.quit()
+
+# For Selenium Grid
 def setup(request):
 
     url=ReadConfigini.read_configuration_file("common info", "url")
@@ -14,15 +47,12 @@ def setup(request):
 
     if browser=="firefox":
         o = FirefoxOptions()
-        driver = Firefox(options=o)
     elif browser=="edge":
         o = EdgeOptions()
-        o.add_experimental_option('detach', True)
-        driver = Edge(options=o)
     else:
         o = ChromeOptions()
-        o.add_experimental_option('detach', True)
-        driver = Chrome(options=o)
+
+    driver = webdriver.Remote(command_executor="http://192.168.1.20:4444/wd/hub", options=o)
 
     wait=WebDriverWait(driver,20)
     testname = request.node.name
@@ -34,4 +64,4 @@ def setup(request):
 
     driver.get(url)
     yield path
-    # driver.quit()
+    driver.quit()
